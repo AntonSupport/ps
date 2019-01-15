b6304f4a8dc8989bde12adcdc974379af3913d24
AntonSupport <lifelink@narod.ru>
2019-01-15 11:37:27 +0300

    Исправлена сессия для виртуального сервера

--- Get-Session.ps1
+++ Get-Session.ps1
@@ -1,14 +1,16 @@
 ﻿
 Param (
-$ComputerName = '192.168.1.64', # Адрес домашнего настольного компьютера A1.
-$UserName = 'Anton'
+    # $ComputerName = '192.168.1.64', # Адрес домашнего настольного компьютера A1.
+    $ComputerName = '192.168.56.3', # Адрес виртуального сервера.
+    $UserName = 'Anton'
 )
 
-if (!$cred)
-{
+if (!$cred -and !$session) {
     Write-Verbose "Запрашиваем логин и пароль к компьютеру"
     New-Variable -Name cred -Value ( Get-Credential -UserName $UserName -Message "Пароль к домашнему компьютеру A1:" ) -Scope "Global"
     New-Variable -Name session -Value ( New-PSSession -ComputerName $ComputerName -Credential $cred ) -Scope "Global"
+}else {
+    Remove-Variable -Name cred, session -Scope "Global"
 }
 
 Get-PSSession
