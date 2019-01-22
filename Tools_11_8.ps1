1478297cf184b610ed040b8686e2216d288e0eba
AntonSupport <lifelink@narod.ru>
2019-01-22 09:05:55 +0300

    Лабораторка на 11 занятие
    Только первая часть лабораторной работы. В книге есть еще одно задание на исправление ошибок.

---
+++ Tools_11_8.ps1
@@ -0,0 +1,12 @@
+﻿Function Get-ServiceInfo {
+    [cmdletbinding()]
+    Param(
+        [string]$Computername)
+
+    $services = Get-WmiObject -Class Win32_Service -filter "state -eq 'Running'" -computername $computername
+    Write-Host "Found ($services.count) on $computername" -ForegroundColor Green
+    $services | sort -Property startname, name | Select -property `
+        startname, name, startmode, computername
+}
+
+Get-ServiceInfo localhost
