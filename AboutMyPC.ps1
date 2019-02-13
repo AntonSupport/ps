782c3d45b110ebfa36e0123ea9550b93fc7fc50f
AntonSupport <lifelink@narod.ru>
2019-02-13 21:37:07 +0300

    Добавил файл AboutMyPC.ps1

---
+++ AboutMyPC.ps1
@@ -0,0 +1,19 @@
+﻿<#
+.SYNOPSIS
+Основная информация о железе
+.DESCRIPTION
+Основная информация:
+Название материнской платы
+Название процессора
+Название оперативки и основные свойства
+Название жесткого диска и его основные свойства
+Другая информация
+#>
+
+
+Write-Host "Материнская плата"
+Get-WmiObject -Class Win32_BaseBoard | Format-Table
+
+Write-Host "Процессор"
+Get-WmiObject -Class CIM_Processor | Select-Object Name, Manufacturer, Caption, MaxClockSpeed, NumberOfCores, NumberOfLogicalProcessors
+
